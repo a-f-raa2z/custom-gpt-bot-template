20585765a1bd572eeba33fd7a2f18a0a37934e80
arazh1123 <arazh1123@gmail.com>
2023-05-11 23:48:16 -0700

    plain gpt bot template

--- README.md
+++ README.md
@@ -1,44 +1,67 @@
-# OpenAI API Quickstart - Python example app
+# For First Time Setup | OpenAI API Quickstart - Python example app
 
 This is an example pet name generator app used in the OpenAI API [quickstart tutorial](https://beta.openai.com/docs/quickstart). It uses the [Flask](https://flask.palletsprojects.com/en/2.0.x/) web framework. Check out the tutorial or follow the instructions below to get set up.
 
-## Setup
+## Setup 
 
-1. If you don’t have Python installed, [install it from here](https://www.python.org/downloads/).
+1. If you don’t have Python installed, [install it from here](https://www.python.org/downloads/)
 
-2. Clone this repository.
+2. Clone this repository
 
-3. Navigate into the project directory:
+3. Navigate into the project directory
 
    ```bash
-   $ cd openai-quickstart-python
+   $ cd gpt-bot
    ```
 
-4. Create a new virtual environment:
+4. Create a new virtual environment
 
    ```bash
    $ python -m venv venv
    $ . venv/bin/activate
    ```
 
-5. Install the requirements:
+5. Install the requirements
 
    ```bash
    $ pip install -r requirements.txt
    ```
 
-6. Make a copy of the example environment variables file:
+6. Make a copy of the example environment variables file
 
    ```bash
    $ cp .env.example .env
    ```
 
-7. Add your [API key](https://beta.openai.com/account/api-keys) to the newly created `.env` file.
+7. Add your [API key](https://beta.openai.com/account/api-keys) to the newly created `.env` file
 
-8. Run the app:
+8. Run the app
 
    ```bash
    $ flask run
    ```
 
 You should now be able to access the app at [http://localhost:5000](http://localhost:5000)! For the full context behind this example app, check out the [tutorial](https://beta.openai.com/docs/quickstart).
+
+# After First Time, please follow the following steps:
+
+1. Navigate into the project directory
+
+   ```bash
+   $ cd /your/path/gpt-bot
+
+2. Start Virtual Environment
+
+   ```bash
+   $ . venv/bin/activate
+
+3. Run Flask
+
+   ```bash
+   $ flask run
+   
+3. Open in browser
+    You should now be able to access the app at [http://localhost:5000](http://localhost:5000)
+   
+
+
--- app.py
+++ app.py
@@ -1,35 +1,43 @@
 import os
-
 import openai
 from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
+chat_history = []
+
+@app.route('/', methods=['GET', 'POST'])
+def knowledge():
+    if request.method == 'POST':
+        global chat_history
+        chat_history = []
+        botknowledge = request.form.get('botknowledge', '')
+        return render_template('index.html', botknowledge=botknowledge, chat_history=chat_history)
+    else:
+        chat_history = []
+        return render_template('index.html', chat_history=chat_history)
+
+@app.route('/ask', methods=['POST'])
+def ask():
+    botknowledge = request.form.get('botknowledge', '')
+    userquestion = request.form['userquestion']
+    chat_history.append({'user': userquestion})
+    prompt = f"{botknowledge}\nQ: {userquestion}\nA:"
+    response = generate_response(prompt)
+    answer = request.args.get("answer")
+    chat_history[-1]['bot'] = response
+    return render_template('index.html', chat_history=chat_history, response=response, answer=answer, botknowledge=botknowledge, userquestion=userquestion)
 
 
-@app.route("/", methods=("GET", "POST"))
-def index():
-    if request.method == "POST":
-        animal = request.form["animal"]
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
-        )
-        return redirect(url_for("index", result=response.choices[0].text))
-
-    result = request.args.get("result")
-    return render_template("index.html", result=result)
-
-
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
+def generate_response(prompt):
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt,
+        max_tokens=200,
+        n=1,
+        stop=None,
+        temperature=0.6,
     )
+
+    message = response.choices[0].text.strip()
+    return message
